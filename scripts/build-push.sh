7ff5daa26737cdc8ab1e36d07e8a5be30d10b10f
Cesar Felipe Reis Alberto <metaces@gmail.com>
2020-07-16 16:03:21 -0300

    Update build-push.sh

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -5,10 +5,10 @@ set -x
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build -t tiagolo/node-frontend:10 .
+docker build -t tiangolo/node-frontend:10 .
 
-docker build -t tiagolo/node-frontend:latest .
+docker build -t tiangolo/node-frontend:latest .
 
-docker push tiagolo/node-frontend:10
+docker push tiangolo/node-frontend:10
 
-docker push tiagolo/node-frontend:latest
+docker push tiangolo/node-frontend:latest
